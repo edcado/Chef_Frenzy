a52da4b153fcfa4674aa9cc71931f5ee02e000ea
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2024-10-22 01:31:39 +0200

    Revert "3: 28: 00"
    This reverts commit 8006a3ab1c7dd0a19b343a373de5d1131bc42749.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,39 +7,11 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] KitchenObjectSO kitchenObjectSO;
     [SerializeField] Transform spawnPoint;
 
-    private KitchenObject kitchenObject;
-    private bool hasSpawned = false;
     public void Interact()
     {
-        if (kitchenObject == null)
-        {
-            if (!hasSpawned)
-            {
-                Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.Prefab, spawnPoint);
-                kitchenObjectTransform.localPosition = Vector3.zero;
-
-                kitchenObjectTransform.GetComponent<KitchenObject>();
-                if (kitchenObject != null)
-                {
-                    kitchenObject.SetClearCounter(this);
-                }
-
-                else
-                {
-                    Debug.Log("Falta Prefab");
-                }
-
-                hasSpawned = true;
-            }
-            
-           
-        }
-
-        else
-        {
-            Debug.Log(kitchenObject.GetClearCounter());
-        }
-        
+        Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.Prefab, spawnPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
 
         
     }
