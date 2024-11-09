a2f9de420f70f7c9c88d9c2e2fcd4f1a57893d08
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2024-11-09 15:34:11 +0100

    plates hold ingredients fixed + platesvisual complete 100%

--- Assets/Scripts/Counters/PlateKitchenObject.cs
+++ Assets/Scripts/Counters/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler <OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     private List<KitchenObjectSO> kitchenObjectSOList;
     [SerializeField] private List<KitchenObjectSO> validIngredientsSOList;
 
@@ -25,8 +32,13 @@ public class PlateKitchenObject : KitchenObject
         }
 
         else
-        {
+        {            
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
        
---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSO_GameObjectList; 
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSO_GameObject in kitchenObjectSO_GameObjectList)
+        {
+            if (kitchenObjectSO_GameObject.kitchenObjectSO == e.kitchenObjectSO)
+            {
+                kitchenObjectSO_GameObject.gameObject.SetActive(true);
+            }
+        }
+    }
+}
