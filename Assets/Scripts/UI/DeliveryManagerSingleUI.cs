c439ef95358e58324a88bdf2b22196c2be47b0fa
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2024-11-15 17:35:01 +0100

    Deliveri UI 100%

--- Assets/Scripts/UI/DeliveryManagerSingleUI.cs
+++ Assets/Scripts/UI/DeliveryManagerSingleUI.cs
@@ -2,13 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DeliveryManagerSingleUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI recipeTextName;
+    [SerializeField] private Transform  iconTemplate;
+    [SerializeField] private Transform iconContainer;
+
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
 
     public void SetRecipeName(RecipeSO recipeSO)
     {
         recipeTextName.text = recipeSO.recipeName;  
+
+        foreach (Transform child in iconContainer)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in recipeSO.kitchenObjectSOList)
+        {
+            Transform iconTransform = Instantiate(iconTemplate, iconContainer);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<Image>().sprite = kitchenObjectSO.sprite;
+        }
     }
+
+    
 }
